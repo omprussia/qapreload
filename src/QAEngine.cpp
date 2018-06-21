f54004b717efe5d6a5dd96437e59119cb1a40078
Alexander Akulich <a.akulich@omprussia.ru>
2018-06-21 16:45:13 +0300

    QAEngine: Fix indention

--- src/QAEngine.cpp
+++ src/QAEngine.cpp
@@ -19,33 +19,33 @@ static QAEngine *s_instance = nullptr;
 
 void QAEngine::initialize()
 {
-        connect(qApp, &QCoreApplication::destroyed, this, [this](){
-            deleteLater();
-        });
+    connect(qApp, &QCoreApplication::destroyed, this, [this](){
+        deleteLater();
+    });
 
-        QTimer::singleShot(5000, qApp, [this](){
-            for (QObject *object : m_objects) {
-                QQuickView *view = qobject_cast<QQuickView*>(object);
-                if (view) {
-                    m_rootItem = view->rootObject();
-                }
-                QQmlApplicationEngine *engine = qobject_cast<QQmlApplicationEngine*>(object);
-                if (engine) {
-                    QQuickWindow *window = qobject_cast<QQuickWindow *>(engine->rootObjects().first());
-                    if (window) {
-                        m_rootItem = window->contentItem();
-                    }
-                }
-                if (m_rootItem) {
-                    qDebug() << Q_FUNC_INFO << "Root item:" << m_rootItem;
-                    QAHooks::removeHooks();
-                    m_objects.clear();
-                    break;
+    QTimer::singleShot(5000, qApp, [this](){
+        for (QObject *object : m_objects) {
+            QQuickView *view = qobject_cast<QQuickView*>(object);
+            if (view) {
+                m_rootItem = view->rootObject();
+            }
+            QQmlApplicationEngine *engine = qobject_cast<QQmlApplicationEngine*>(object);
+            if (engine) {
+                QQuickWindow *window = qobject_cast<QQuickWindow *>(engine->rootObjects().first());
+                if (window) {
+                    m_rootItem = window->contentItem();
                 }
             }
+            if (m_rootItem) {
+                qDebug() << Q_FUNC_INFO << "Root item:" << m_rootItem;
+                QAHooks::removeHooks();
+                m_objects.clear();
+                break;
+            }
+        }
 
-            QAService::instance()->initialize(QStringLiteral("ru.omprussia.qaservice.%1").arg(qApp->applicationFilePath().section(QChar('/'), -1)));
-        });
+        QAService::instance()->initialize(QStringLiteral("ru.omprussia.qaservice.%1").arg(qApp->applicationFilePath().section(QChar('/'), -1)));
+    });
 }
 
 void QAEngine::addObject(QObject *o)
