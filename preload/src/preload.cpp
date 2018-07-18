f537462afbbab328b6af32460ec57dd5d2093311
Andrey Kozhevnikov <a.kozhevnikov@omprussia.ru>
2018-07-18 11:25:43 +0300

    Allow loading next startup hooks

--- preload/src/preload.cpp
+++ preload/src/preload.cpp
@@ -8,6 +8,10 @@ unsigned int qtHookData[100];
 static int qa_loaded = 0;
 static int is_nemo = 0;
 
+static void (*next_startup_hook)() = nullptr;
+
+typedef void(*StartupCallback)();
+
 inline uid_t nemo_uid()
 {
     static struct passwd *nemo_pwd;
@@ -32,12 +36,20 @@ extern "C" void qt_startup_hook()
     }
     dlopen("libqaengine.so", RTLD_LAZY);
     qa_loaded = 1;
+
+    static void(*next_qt_startup_hook)() = (void (*)()) dlsym(RTLD_NEXT, "qt_startup_hook");
+    next_qt_startup_hook();
+
+    if (next_startup_hook) {
+        next_startup_hook();
+    }
 }
 
 __attribute__((constructor))
 static void libConstructor() {
     is_nemo = getuid() == nemo_uid();
     if (is_nemo) {
+        next_startup_hook = reinterpret_cast<StartupCallback>(qtHookData[5]);
         qtHookData[5] = reinterpret_cast<unsigned int>(&qt_startup_hook);
     }
 }
